7f9d33e041d82a2ba8dec9d8c067f68ddefdcd45
palamaramt <palamaramt@gmail.com>
2022-02-09 14:38:10 -0700

    Renamed a function and added csv appending abilities.

--- PetFoodSourcerer.py
+++ PetFoodSourcerer.py
@@ -1,9 +1,31 @@
+# import specific libraries
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from datetime import date
+# import libraries
 import time
+import re
+import csv
+import sys
+# get custom functions
+from appendToCSV import appendToCSV
+from priceFinder import priceFinder
+from checkCSV import checkCSV
 
-from price_finder import price_finder
+# get today's date
+today = date.today().strftime("%m/%d/%y")
+# make empty dictionary for completed date
+pricesList= []
+# looking for cat or dog food?
+species = "dog"
+
+# Check if prices have been checked already
+filename = "csvs/dogFoodPriceRecords.csv"
+checkCSV(filename,today)
+
+# get only numbers
+numbers = re.compile(r'\d+(?:\.\d+)?')
 
 # list of website: petco, petsmart
 websites = {
@@ -11,10 +33,28 @@ websites = {
     'PetSmart': ['https://www.petsmart.com/dog/food/dry-food/blue-buffalo-life-protection-formula-adult-dry-dog-food---chicken-and-brown-rice-41846.html?cgid=100246','span.product-price-discount']
     }
 
+# change path to match your directory structure for the geckodriver
 gecko_path = "/Users/Marissa/AppData/Local/Programs/Python/Python39/geckodriver.exe"
 service = Service(executable_path=gecko_path)
 
+# iterate through the websites dictionary and find the prices listed on the websites
 for name,data in websites.items():
+    # dictionary with empty values
+    myDict = {"Name": [], "Price": [], "Date": []}
+    # get website url
     website = data[0]
+    # get what specifically you are looking for in the website
     css = data[1]
-    price_finder(service,website,name,css)
+    # use priceFinder.py to find the price
+    price = priceFinder(service,website,name,css)
+    # strip any words and only get numbers for the prices
+    price = numbers.findall(price)
+    # put name of website, the price, and the date into dictionary
+    myDict["Name"] = name
+    myDict["Price"] = price[0]
+    myDict["Date"] = today
+    # append the dictionary to the list of prices
+    pricesList.append(myDict)
+
+# call appendToCSV.py to append data to end of csv
+appendToCSV(pricesList,species,today,filename)
---
+++ checkCSV.py
@@ -0,0 +1,31 @@
+import csv
+import sys
+
+
+def checkCSV(filename,today):
+    # open the csv file
+    with open(filename, 'r') as csvfile:
+        # read the csv file
+        csvfile = csv.reader(csvfile)
+        # create empty variable for continuation
+        cont = ""
+        # go through all lines in the csv
+        for lines in csvfile:
+            # if today's date is found in the csv, create following options:
+            if lines[-1] == today:
+                # loop to force user to choose to continue or not
+                while cont.lower() is not "n" or "y":
+                    print("You've already searched for prices today!")
+                    cont = input("Would you like to continue? (Y/N): ")
+                    # if user chooses to not continue, end program
+                    if cont.lower() == "n":
+                        sys.exit("Ending program.")
+                    # if suer chooses to continue, exit function
+                    elif cont.lower() == "y":
+                        print("Continuing program.")
+                        return
+                    # if user makes invalid choice, tell them about it
+                    else:
+                        print("Please enter a valid answer.")
+            else:
+                return
