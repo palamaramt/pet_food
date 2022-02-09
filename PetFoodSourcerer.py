5112d48452ebb43411d7a7efffdea2aa66ae47b9
palamaramt <palamaramt@gmail.com>
2022-02-09 10:24:13 -0700

    Fixes warnings

--- PetFoodSourcerer.py
+++ PetFoodSourcerer.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 
 website = 'https://www.petco.com/shop/en/petcostore/product/blue-buffalo-chicken-and-brown-rice-adult-dog-food'
 
-browser = webdriver.Firefox(executable_path="/Users/Marissa/AppData/Local/Programs/Python/Python39/geckodriver.exe")
+gecko_path = "/Users/Marissa/AppData/Local/Programs/Python/Python39/geckodriver.exe"
+service = Service(executable_path=gecko_path)
+browser = webdriver.Firefox(service = service)
 browser.get(website)
 
-price = browser.find_element_by_css_selector('div.PriceRowContainer-sc-1wlo6zv-1.fPOcHS')
-print("Price: " + price.text)
+price = browser.find_element(By.CSS_SELECTOR, 'div.PriceRowContainer-sc-1wlo6zv-1.fPOcHS')
+#price = browser.find_element_by_css_selector('div.PriceRowContainer-sc-1wlo6zv-1.fPOcHS')
+print("Petco Price: " + price.text)
 
 time.sleep(3)
 browser.close()
+browser.quit()
